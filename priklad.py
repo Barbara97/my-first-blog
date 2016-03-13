4d4ceff24798e49c9ad4b4dd5d8c8efde353e5e6
Babu <baskabobenicova@gmail.com>
2016-03-13 12:42:46 +0100

    nasa prva verzia

---
+++ priklad.py
@@ -0,0 +1,14 @@
+def x(a,b):
+    return a+b
+a=9
+if a>=0 and a<=5:
+    print ('0-5')
+elif a>5 and a<10:
+    print ('5-10')
+else:
+    print ('ani jedno')
+print ('koniec')
+print (x(4,8))
+y=[4,8,1,9,2,14,5,74,3]
+for a in y:
+    print (a)
